3acf2bb7e9ae023d807676f1ddccd11d46781ce1
blckshdw <danhaas@gmail.com>
2016-06-18 08:12:18 -0400

    Add Request & Response classes

---
+++ TransmissionFluid/TransmissionRemote.RPC/TransmissionRequest.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransmissionRemote.RPC
+{
+    public class TransmissionRequest
+    {
+        [JsonProperty(PropertyName = "method")]
+
+        public string Method { get; set; }
+        [JsonProperty(PropertyName = "tag")]
+
+        public int Tag { get; set; }
+        [JsonProperty(PropertyName = "arguments")]
+        public Dictionary<string, object> Arguments { get; set; }
+
+
+        public TransmissionRequest() { }
+        public TransmissionRequest(string method)
+        {
+            this.Method = method;
+        }
+        public TransmissionRequest(string method, Dictionary<string, object> args)
+        {
+            this.Method = method;
+            this.Arguments = args;
+        }
+    }
+}
---
+++ TransmissionFluid/TransmissionRemote.RPC/TransmissionResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransmissionRemote.RPC
+{
+    public class TransmissionResponse
+    {
+        [JsonProperty(PropertyName = "arguments")]
+        public Dictionary<string, object> Arguments { get; set; }
+
+        [JsonProperty(PropertyName = "result")]
+        public string Result { get; set; }
+
+        [JsonProperty(PropertyName = "tag")]
+        public string Tag { get; set; }
+
+        public T Deserialize<T>()
+        {
+            var arguments = JsonConvert.SerializeObject(this.Arguments);
+            return JsonConvert.DeserializeObject<T>(arguments);
+        }
+    }
+}
